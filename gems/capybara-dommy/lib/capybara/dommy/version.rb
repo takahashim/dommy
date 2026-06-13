53e175509686fe71b43eef4d0dbb1a200992fcaa
takahashim <takahashimm@gmail.com>
2026-06-13 23:58:18 +0900

    chore: bump version to 0.9.0

--- gems/capybara-dommy/lib/capybara/dommy/version.rb
+++ gems/capybara-dommy/lib/capybara/dommy/version.rb
@@ -2,6 +2,6 @@
 
 module Capybara
   module Dommy
-    VERSION = "0.8.0"
+    VERSION = "0.9.0"
   end
 end
--- gems/dommy-rack/lib/dommy/rack/version.rb
+++ gems/dommy-rack/lib/dommy/rack/version.rb
@@ -2,6 +2,6 @@
 
 module Dommy
   module Rack
-    VERSION = "0.8.0"
+    VERSION = "0.9.0"
   end
 end
